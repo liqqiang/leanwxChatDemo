84b033ded67747b9d350ef3cf3132d4f4212db65
liqiang <liqiang@bis.com.cn>
2017-03-28 18:01:39 +0800

    wx jssdk token update

--- wxconfig.js
+++ wxconfig.js
@@ -1,9 +1,9 @@
 wx.config({
 	debug: true,
 	appId: "wx34abe5391c3fe026",
-	timestamp: 1490665592,
-	nonceStr: "bed1ea74-6585-4de4-817a-8efdacae3910",
-	signature: "95f493d2e9871d2e991bfc478c5ea1e37848d163",
+	timestamp: 1490695179,
+	nonceStr: "d93533ba-0fd5-430d-8018-8f3869608df8",
+	signature: "0439ba5bc05a7fe97aa78a48a07478dfa490bef4",
 	jsApiList:[
 	  "startRecord",
 	  "stopRecord",
